c093c9f29b0b0e3612cafc4c9ddc6cd78909096c
CC11001100 <CC11001100@qq.com>
2025-04-12 03:04:12 +0800

    将ChallengeList组件重命名为ChallengeListPage并更新相关引用

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,30 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
 import HomePage from './components/HomePage'
-import ChallengeList from './components/ChallengeList'
+import ChallengeListPage from './components/ChallengeListPage'
 import ChallengeDetailPage from './components/ChallengeDetailPage'
 import AboutPage from './components/AboutPage'
 import './gh-fork-ribbon.css';
 
-function App() {
+const App = () => {
     return (
         <Router>
-            <div className="app-container">
+            <div className="App">
+                <div className="github-fork-ribbon-wrapper right">
+                    <div className="github-fork-ribbon">
+                        <a href="https://github.com/JSREP/crawler-leetcode" target="_blank" rel="noopener noreferrer">Fork me on GitHub</a>
+                    </div>
+                </div>
                 <NavBar/>
-                <Routes>
-                    <Route path="/" element={<HomePage/>}/>
-                    <Route path="/challenges" element={<ChallengeList/>}/>
-                    <Route path="/challenge/:id" element={<ChallengeDetailPage/>}/>
-                    <Route path="/about" element={<AboutPage/>}/>
-                </Routes>
+                <div style={{ padding: '20px' }}>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/challenges" element={<ChallengeListPage/>}/>
+                        <Route path="/challenge/:id" element={<ChallengeDetailPage/>}/>
+                        <Route path="/about" element={<AboutPage/>}/>
+                        <Route path="*" element={<HomePage/>}/>
+                    </Routes>
+                </div>
             </div>
         </Router>
     )
