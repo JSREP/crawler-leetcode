4eb021349abced1ff28a95e12c35e5d9dfdb4042
CC11001100 <CC11001100@qq.com>
2025-04-13 09:46:33 +0800

    fix: 修改百度统计实现方式，直接在HTML中加载

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import GitHubRibbon from './components/GitHubRibbon'
 import PageTitle from './components/PageTitle'
 import './gh-fork-ribbon.css';
 import './styles/github-ribbon-fix.css';
-import BaiduAnalytics from './components/BaiduAnalytics'
 
 const App = () => {
     return (
@@ -18,7 +17,6 @@ const App = () => {
             <div className="App">
                 <PageTitle />
                 <GitHubRibbon repositoryUrl="https://github.com/JSREP/crawler-leetcode" />
-                <BaiduAnalytics siteId="b4fcc22834ded2c9864e661e6a5a634d" />
                 <NavBar/>
                 <div className="content-wrapper" style={{ padding: '20px 0' }}>
                     <Routes>
