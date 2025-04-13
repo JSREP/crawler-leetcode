49c9d59bd9ca5218d4ffaf3732b4a0791ad3f0ff
CC11001100 <CC11001100@qq.com>
2025-04-13 09:35:23 +0800

    feat: 集成百度统计

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import GitHubRibbon from './components/GitHubRibbon'
 import PageTitle from './components/PageTitle'
 import './gh-fork-ribbon.css';
 import './styles/github-ribbon-fix.css';
+import BaiduAnalytics from './components/BaiduAnalytics'
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
             <div className="App">
                 <PageTitle />
                 <GitHubRibbon repositoryUrl="https://github.com/JSREP/crawler-leetcode" />
+                <BaiduAnalytics siteId="b4fcc22834ded2c9864e661e6a5a634d" />
                 <NavBar/>
                 <div className="content-wrapper" style={{ padding: '20px 0' }}>
                     <Routes>
